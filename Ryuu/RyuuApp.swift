73053220328253c720020d330842a85612b1442f
sachin.r.singh <sachin.r.singh@accenture.com>
2025-07-02 12:55:56 +0300

    Initial Commit

---
+++ Ryuu/RyuuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RyuuApp.swift
+//  Ryuu
+//
+//  Created by sachin.r.singh on 02/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct RyuuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
